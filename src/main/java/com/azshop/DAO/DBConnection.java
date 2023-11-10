d5580987ba6b84c1ed487c6b334565afc5cb23c3
BaoNun1205 <baole.120503@gmail.com>
2023-11-10 14:26:15 +0700

    thêm service impl product

--- src/main/java/com/azshop/DAO/DBConnection.java
+++ src/main/java/com/azshop/DAO/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-	private final String serverName = "THANH-HUY";
+	private final String serverName = "DESKTOP-NN1DVIA";
 	private final String dbName = "AZShop_Nhom_4";
 	private final String portNumber = "1433";
 	private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
---
+++ src/main/java/com/azshop/services/ProductServiceImpl.java
@@ -0,0 +1,52 @@
+package com.azshop.services;
+
+import java.util.List;
+
+import com.azshop.DAO.IProductDAO;
+import com.azshop.DAO.ProductDAOImpl;
+import com.azshop.models.ProductModel;
+
+public class ProductServiceImpl implements IProductService{
+
+	IProductDAO productDAO = new ProductDAOImpl();
+	@Override
+	public void insert(ProductModel product) {
+		productDAO.insert(product);
+	}
+
+	@Override
+	public ProductModel getById(int id) {
+		return productDAO.getById(id);
+	}
+
+	@Override
+	public List<ProductModel> getAll() {
+		return productDAO.getAll();
+	}
+
+	@Override
+	public List<ProductModel> getByCategoryId(int categoryId) {
+		return productDAO.getByCategoryId(categoryId);
+	}
+
+	@Override
+	public List<ProductModel> getByStyleValueId(int styleValueId) {
+		return productDAO.getByStyleValueId(styleValueId);
+	}
+
+	@Override
+	public List<ProductModel> getByStoreId(int storeId) {
+		return productDAO.getByStoreId(storeId);
+	}
+
+	@Override
+	public void update(ProductModel product) {
+		productDAO.update(product);
+	}
+
+	@Override
+	public void delete(int id) {
+		productDAO.delete(id);
+	}
+
+}
