61db099e95a1fde0f4a640da4d81ca7419d55d1e
BaoNun1205 <baole.120503@gmail.com>
2023-11-10 15:19:37 +0700

    them getall trong controller guest

--- src/main/java/com/azshop/controllers/guest/GuestController.java
+++ src/main/java/com/azshop/controllers/guest/GuestController.java
@@ -1,6 +1,7 @@
 package com.azshop.controllers.guest;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,23 +10,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.azshop.models.ProductModel;
+import com.azshop.services.IProductService;
+import com.azshop.services.ProductServiceImpl;
+
 @WebServlet(urlPatterns = {"/guest-home"})
 public class GuestController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
+	IProductService productService = new ProductServiceImpl();
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI().toString();
 		if (url.contains("guest-home")) {
 			try {
-				getCollections(req, resp);
+				getAllProduct(req, resp);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	private void getCollections(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	private void getAllProduct(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<ProductModel> listProduct = productService.getAll();
+		
+		req.setAttribute("listproduct", listProduct);
 		RequestDispatcher rd = req.getRequestDispatcher("/views/guest/home.jsp");
 		rd.forward(req, resp);
 	}
