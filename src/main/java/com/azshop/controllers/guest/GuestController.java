8357e8d47e2db7fd1a6ba8f3e0a27502d41d856a
BaoNun1205 <baole.120503@gmail.com>
2023-11-10 19:35:59 +0700

    thêm upload ảnh

--- src/main/java/com/azshop/controllers/guest/GuestController.java
+++ src/main/java/com/azshop/controllers/guest/GuestController.java
@@ -1,7 +1,9 @@
 package com.azshop.controllers.guest;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,8 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.azshop.models.ImageModel;
 import com.azshop.models.ProductModel;
+import com.azshop.services.IImageService;
 import com.azshop.services.IProductService;
+import com.azshop.services.ImageServiceImpl;
 import com.azshop.services.ProductServiceImpl;
 
 @WebServlet(urlPatterns = {"/guest-home"})
@@ -20,6 +25,7 @@ public class GuestController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	IProductService productService = new ProductServiceImpl();
+	IImageService imageService = new ImageServiceImpl();
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI().toString();
@@ -33,9 +39,18 @@ public class GuestController extends HttpServlet{
 	}
 
 	private void getAllProduct(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		List<ProductModel> listProduct = productService.getAll();
+		List<ProductModel> productList = productService.getAll();
 		
-		req.setAttribute("listproduct", listProduct);
+		// Tạo một Map để lưu trữ danh sách ảnh của từng sản phẩm.
+		Map<Long, List<ImageModel>> productImagesMap = new HashMap<>();
+		
+		for (ProductModel product : productList) {
+			List<ImageModel> productImages = imageService.getByProductId(product.getId());
+		    productImagesMap.put((long) product.getId(), productImages);
+		}
+		
+		req.setAttribute("listproduct", productList);
+		req.setAttribute("productImagesMap", productImagesMap);
 		RequestDispatcher rd = req.getRequestDispatcher("/views/guest/home.jsp");
 		rd.forward(req, resp);
 	}
