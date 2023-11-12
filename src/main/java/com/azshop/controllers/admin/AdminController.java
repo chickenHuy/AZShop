2625799d9f08edb03ec8ab6fcacac9f01ad371f5
phuon <phuon@DESKTOP-2PP94Q3>
2023-11-12 23:14:10 +0700

    add admin template

--- src/main/java/com/azshop/controllers/admin/AdminController.java
+++ src/main/java/com/azshop/controllers/admin/AdminController.java
@@ -1,5 +1,27 @@
 package com.azshop.controllers.admin;
 
-public class AdminController {
-	// Some code
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = {"/admin/dashboard"})
+public class AdminController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String url = req.getRequestURL().toString();
+		if (url.contains("/admin/dashboard")) {
+			RequestDispatcher rDispatcher = req.getRequestDispatcher("/views/admin/dashboard.jsp");
+			rDispatcher.forward(req, resp);
+		}
+	}
+
 }
