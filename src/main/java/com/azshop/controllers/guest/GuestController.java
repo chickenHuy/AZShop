952af5eecdaa5d00352f12b1a0a6a44bae02e63f
hieulag15 <hieuthanhtran12@gmail.com>
2023-11-14 21:15:27 +0700

    tao controller khac

--- src/main/java/com/azshop/controllers/guest/GuestController.java
+++ src/main/java/com/azshop/controllers/guest/GuestController.java
@@ -26,7 +26,7 @@ import com.azshop.services.ProductServiceImpl;
 import com.azshop.services.UserServiceImpl;
 import com.azshop.utils.Email;
 
-@WebServlet(urlPatterns = {"/guest-home", "/guest-clothing", "/register", "/VerifyCode"})
+@WebServlet(urlPatterns = {"/guest-home", "/guest-clothing"})
 public class GuestController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
@@ -51,20 +51,6 @@ public class GuestController extends HttpServlet{
 				e.printStackTrace();
 			}
 		}
-		else if (url.contains("register")) {
-			try {
-				getRegister(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		else if (url.contains("VerifyCode")) {
-			try {
-				getVerifyCode(req, resp);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
 	}
 	
 	private void getAllClothing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -77,81 +63,6 @@ public class GuestController extends HttpServlet{
 		rd.forward(req, resp);
 	}
 
-	private void getVerifyCode(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/views/account/Verify1.jsp").forward(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String url = req.getRequestURI().toString();
-		if (url.contains("register")) {
-			postRegister(req, resp);
-		} else if (url.contains("VerifyCode")) {
-			postVerify(req, resp);
-		}
-	}
-
-	private void postVerify(HttpServletRequest req, HttpServletResponse resp) {
-		resp.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter outPrintWriter = resp.getWriter()){
-			
-			HttpSession session = req.getSession();
-			UserModel userModel = (UserModel) session.getAttribute("user");
-			String codeSend = (String) session.getAttribute("code");
-			
-			String code = req.getParameter("verify-code");
-			
-			if (code.equals(codeSend)) {
-				userModel.setEmail(userModel.getEmail());
-				userModel.setEmailActive(true);
-				
-				userService.updateStatusEmail(userModel);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void postRegister(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
-		resp.setCharacterEncoding("UTF-8");
-		req.setCharacterEncoding("UTF-8");
-		String email = req.getParameter("email");
-		String password = req.getParameter("password");
-		String firstName = req.getParameter("first-name");
-		String lastName = req.getParameter("last-name");
-		
-		if (userService.checkExistEmial(email)) {
-			resp.sendRedirect(req.getContextPath() + "/views/account/register1.jsp");
-		} else {
-			Email mail = new Email();
-			String code = mail.getRandom();
-			
-			UserModel user = new UserModel(firstName, lastName, email);
-			
-			boolean test = mail.sendEmail(user, code);
-			if (test) {
-				HttpSession session = req.getSession();
-				session.setAttribute("user", user);
-				session.setAttribute("code", code);
-				
-				boolean isSuccess = userService.insertRegister(firstName, lastName, email, password);
-				
-				if (isSuccess) {
-					resp.sendRedirect(req.getContextPath() + "/VerifyCode");
-				} else {
-					req.getRequestDispatcher("/views/account/register1.jsp").forward(req, resp);
-				}
-			} else {
-				PrintWriter outPrintWriter = resp.getWriter();
-				outPrintWriter.println("Loi khi gui mail!");
-			}
-		}
-	}
-
-	private void getRegister(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/views/account/register1.jsp").forward(req, resp);
-	}
 
 	private void getAllProduct(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
