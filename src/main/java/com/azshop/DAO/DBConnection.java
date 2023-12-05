46c96113a964f8c4e64f1fc3415457b9d59dccb7
= <=>
2023-12-05 18:02:06 +0700

    oke

--- src/main/java/com/azshop/DAO/DBConnection.java
+++ src/main/java/com/azshop/DAO/DBConnection.java
@@ -5,12 +5,8 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-<<<<<<< HEAD
 	private final String serverName = "HIEULAG\\THANHHIEU";//"DESKTOP-NN1DVIA"; 
 
-=======
-	private final String serverName = "TLNKHANG";//"HIEULAG\\THANHHIEU";//"DESKTOP-NN1DVIA"; 
->>>>>>> 28e98413fe5ad0569a13a2c9b5e51d55799129be
 	private final String dbName = "AZShop_Nhom_4";
 	private final String portNumber = "1433";
 	private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
