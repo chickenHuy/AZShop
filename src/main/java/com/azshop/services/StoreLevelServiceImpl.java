75f9d24733d63e92f4076336298d61e28dd8f4f7
chuot <chuot@192.168.75.1>
2023-12-05 19:05:56 +0700

    them service

---
+++ src/main/java/com/azshop/services/StoreLevelServiceImpl.java
@@ -0,0 +1,39 @@
+package com.azshop.services;
+
+import java.util.List;
+
+import com.azshop.DAO.IStoreLevelDAO;
+import com.azshop.DAO.StoreLevelDAOImpl;
+import com.azshop.models.StoreLevelModel;
+
+public class StoreLevelServiceImpl implements IStoreLevelService{
+
+	IStoreLevelDAO storeLevelDAO = new StoreLevelDAOImpl();
+	@Override
+	public void insert(StoreLevelModel storeLevel) {
+		storeLevelDAO.insert(storeLevel);
+		
+	}
+
+	@Override
+	public StoreLevelModel getById(int id) {
+		return storeLevelDAO.getById(id);
+	}
+
+	@Override
+	public List<StoreLevelModel> getAll() {
+		return storeLevelDAO.getAll();
+	}
+
+	@Override
+	public void update(StoreLevelModel storeLevel) {
+		
+		storeLevelDAO.update(storeLevel);
+	}
+
+	@Override
+	public void delete(int id) {
+		storeLevelDAO.delete(id);
+	}
+
+}
