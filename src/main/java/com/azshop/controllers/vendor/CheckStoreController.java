0d90091a65b7d5840dc3782196ed957589638c27
Khan9Tran <trankhangtctv@gmail.com>
2023-12-06 10:07:47 +0700

    fix

--- src/main/java/com/azshop/controllers/vendor/CheckStoreController.java
+++ src/main/java/com/azshop/controllers/vendor/CheckStoreController.java
@@ -10,26 +10,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.azshop.models.StoreModel;
 import com.azshop.models.UserModel;
 import com.azshop.services.IStoreService;
 import com.azshop.services.StoreServiceImpl;
 import com.azshop.utils.Constant;
 
 @WebServlet(urlPatterns = { "/register-shop", "/store-check"})
-public class VendorLoginController extends HttpServlet{
+public class CheckStoreController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	IStoreService storeService = new StoreServiceImpl();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
-		HttpSession session = req.getSession();
-	    UserModel userModel = (UserModel) session.getAttribute(Constant.userSession);
 		String url = req.getRequestURL().toString();
+		HttpSession session = req.getSession();
 		if (url.contains("/store-check")) {
-			RequestDispatcher rDispatcher = req.getRequestDispatcher("/views/vendor/createShop.jsp");
-			rDispatcher.forward(req, resp);
-			return;
+			
+		    UserModel userModel = (UserModel) session.getAttribute(Constant.userSession);
+		    if (userModel == null)
+		    {
+		    	RequestDispatcher rDispatcher = req.getRequestDispatcher("/views/account/login.jsp");
+				rDispatcher.forward(req, resp);
+		    }
+		    else {
+			    StoreModel storeModel = storeService.getByOwnerId(userModel.getId());
+			    if (storeModel == null) {
+			    	req.setAttribute("userModel", userModel);
+			    	RequestDispatcher rDispatcher = req.getRequestDispatcher("/views/vendor/createShop.jsp");
+					rDispatcher.forward(req, resp);
+				}
+			    else {
+			        session.setAttribute(Constant.storeSession, storeModel);
+			    	RequestDispatcher rDispatcher = req.getRequestDispatcher("/views/vendor/dashboard.jsp");
+					rDispatcher.forward(req, resp);
+					
+				}
+		    }
 		}
 	}
 	
