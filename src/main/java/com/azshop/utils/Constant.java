c731350239e676200b53e04b1445279e9de1b12e
hieulag15 <hieuthanhtran12@gmail.com>
2023-12-06 09:45:19 +0700

    fix session

--- src/main/java/com/azshop/utils/Constant.java
+++ src/main/java/com/azshop/utils/Constant.java
@@ -5,6 +5,5 @@ import javax.servlet.http.HttpSession;
 public class Constant {
 	public static final String DIR ="D:\\uploads";
 	
-	public static HttpSession SESSION = null; 
-	public static String userSession = "User";
+	public static String userSession = "Account";
 }
