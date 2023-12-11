b2756361d540c0751b08ca248f338a81eb581bcb
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-12-11 11:52:21 +0700

    Get lastet

--- src/main/java/com/azshop/DAO/DBConnection.java
+++ src/main/java/com/azshop/DAO/DBConnection.java
@@ -4,23 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-<<<<<<< HEAD
-//
     private final String serverName = "THANH-HUY";
     private final String dbName = "AZShop_Nhom_3";
     private final String portNumber = "1433";
     private final String username = "sa";
     private final String password = "Httt2807";
-=======
 
-    private final String serverName = "HIEULAG\\THANHHIEU";
-
-    private final String dbName = "AZShop_Nhom_3";
-    private final String portNumber = "1433";
-    private final String username = "sa";
-    private final String password = "123456";
->>>>>>> c7874bdc9b50cb74c294089b240f6bad81377063
-    
     
     public Connection getConnection() throws Exception {
         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
