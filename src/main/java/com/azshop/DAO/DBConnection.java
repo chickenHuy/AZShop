36d8ccd649cf48edb83cba487081a5ca931d4831
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-12-11 11:58:43 +0700

    Get update

--- src/main/java/com/azshop/DAO/DBConnection.java
+++ src/main/java/com/azshop/DAO/DBConnection.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-<<<<<<< HEAD
     private final String serverName = "THANH-HUY";
     private final String dbName = "AZShop_Nhom_3";
     private final String portNumber = "1433";
@@ -18,7 +17,6 @@ public class DBConnection {
         return DriverManager.getConnection(url, username, password);
     }
     
-=======
 //
 //    private final String serverName = "database-azshop.cvv8fenhkivu.ap-southeast-2.rds.amazonaws.com";
 //    private final String dbName = "AZShop_Nhom_3";
@@ -32,24 +30,7 @@ public class DBConnection {
 //        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 //        return DriverManager.getConnection(url, username, password);
 //    }
-//    
-	private final String serverName = "TLNKHANG";
-	private final String dbName = "AZShop_Nhom_3";
-	private final String portNumber = "1433";
-	private final String instance = "";// MSSQLSERVER LEAVE THIS ONE
-	
-	public Connection getConnection() throws Exception {
-		String url = "jdbc:sqlserver://" + serverName + ":" + portNumber
-		+ "\\" + instance + ";integratedSecurity=true;databaseName=" +
-		dbName;
-		if (instance == null || instance.trim().isEmpty())
-		url = "jdbc:sqlserver://" + serverName + ":" + portNumber +
-		";integratedSecurity=true;databaseName=" + dbName;
-		//DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		return DriverManager.getConnection(url);
-		}
->>>>>>> 4d063a5e7ee326825597200262dad20f69ce5600
+
 	public static void main(String[] args) {
 
 		try {
